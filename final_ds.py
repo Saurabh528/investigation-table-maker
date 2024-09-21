be939fd19da807ff13b0dc7755cdd71152312e73
Saurabh528 <48671234+Saurabh528@users.noreply.github.com>
2024-09-21 23:43:35 +0530

    Update final_ds.py

--- final_ds.py
+++ final_ds.py
@@ -9,7 +9,7 @@ import re
 from streamlit_markmap import markmap
 import json
 from investigations import create_patient_document
-from streamlit_mic_recorder import mic_recorder, speech_to_text
+#from streamlit_mic_recorder import mic_recorder, speech_to_text
 
 
 ASSISTANT_ID = "asst_wTfRitYZNaIGHByiYw2SmQrn"
